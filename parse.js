b004c48a3ca94730be22da04aa756d5fbef9f26b
tRoettger <tobias.roettger@gmx.net>
2024-05-02 15:13:50 +0200

    Parses html to json

--- parse.js
+++ parse.js
@@ -18,3 +18,42 @@ for(let argv of process.argv) {
     processor(argv);
 }
 
+const REGEX_NAME = /\<th\scolspan\=\"2\"\>(?<name>.*?)\<\/th\>/;
+const REGEX_ROW = /\<tr\>(?<content>.*?)\<\/tr\>/g;
+const REGEX_MISSION = /(?<planet>[^\/]*)\/(?<name>[^\()]*)\((?<type>[^\)]*)/;
+const REGEX_REWARD = /\<td\>(?<item>[\s\S]*)\<\/td\>\<td\>(?<rarity>[a-zA-Z]*)\s\((?<properbility>.*)\%\)\<\/td\>/;
+
+const mission = {};
+for( let match of line.matchAll(REGEX_ROW)) {
+    let nameMatch = match.groups.content.match(REGEX_NAME);
+    if(nameMatch) {
+        if(mission.name) {
+            mission.rotations ??= [];
+            mission.rotations.push({ name: nameMatch.groups.name });
+        } else {
+            let missionMatch = nameMatch.groups.name.match(REGEX_MISSION);
+            mission.name = missionMatch.groups.name.trim();
+            mission.planet = missionMatch.groups.planet.trim();
+            mission.type = missionMatch.groups.type.trim();
+        }
+    } else {
+        let rewardMatch = match.groups.content.match(REGEX_REWARD);
+        if(rewardMatch) {
+            let rewards;
+            if(mission.rotations) {
+                let rota = mission.rotations[mission.rotations.length - 1];
+                rota.rewards ??= [];
+                rewards = rota.rewards;
+            } else {
+                mission.rewards ??= [];
+                rewards = mission.rewards;
+            }
+            rewards.push({
+                item: rewardMatch.groups.item,
+                rarity: rewardMatch.groups.rarity,
+                properbility: rewardMatch.groups.properbility * 1
+            });
+        }
+    }
+}
+console.log(JSON.stringify(mission));
--- parse.sh
+++ parse.sh
@@ -4,7 +4,6 @@ CRAWLED_FILE=$1
 echo "Starting node..."
 while IFS= read -r line
 do
-    #node parse.js --line "$line"
-    echo $line
+    node parse.js --line "$line"
 done < $CRAWLED_FILE
 echo "Node finished"
