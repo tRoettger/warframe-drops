51c8cc126c884dc3f8bfbabf53808ee92c41c986
tRoettger <tobias.roettger@gmx.net>
2024-05-02 15:17:45 +0200

    Create real json array output

--- parse.sh
+++ parse.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 CRAWLED_FILE=$1
-echo "Starting node..."
+FIRST=true
+echo "["
 while IFS= read -r line
 do
+    if $FIRST
+    then
+        FIRST=false
+    else
+        echo ","
+    fi
     node parse.js --line "$line"
 done < $CRAWLED_FILE
-echo "Node finished"
+echo "]"
