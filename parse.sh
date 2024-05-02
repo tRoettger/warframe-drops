13d4ca00d3c0f5a190c6acc3c8960ea82c4171da
tRoettger <tobias.roettger@gmx.net>
2024-05-02 14:29:42 +0200

    Fixes EOF error

--- parse.sh
+++ parse.sh
@@ -4,6 +4,7 @@ CRAWLED_FILE=$1
 echo "Starting node..."
 while IFS= read -r line
 do
-    node parse.js --line "$line"
+    #node parse.js --line "$line"
+    echo $line
 done < $CRAWLED_FILE
-echo "Node finished
+echo "Node finished"
